1c9cef30504f6e3e556415b91d4528a6c5ea1534
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2018-11-26 17:05:30 -0500

    init, completed

--- tuneup.py
+++ tuneup.py
@@ -2,16 +2,24 @@
 # -*- coding: utf-8 -*-
 """Tuneup assignment"""
 
-__author__ = "???"
+__author__ = "Jake Johnson"
 
 import cProfile
 import pstats
+import timeit
+from collections import Counter
 
 
 def profile(func):
     """A function that can be used as a decorator to meausre performance"""
-
-    raise NotImplementedError("Complete this decorator function")
+    def function_wrapper(x):
+        pr = cProfile.Profile()
+        pr.enable()
+        func(x)
+        pr.disable()
+        stats = pstats.Stats(pr).sort_stats('cumulative')
+        stats.print_stats()
+    return function_wrapper
 
 
 def read_movies(src):
@@ -21,35 +29,32 @@ def read_movies(src):
         return f.read().splitlines()
 
 
-def is_duplicate(title, movies):
-    """Case insensitive search within a list"""
-    for movie in movies:
-        if movie.lower() == title.lower():
-            return True
-    return False
-
-
+@profile
 def find_duplicate_movies(src):
     """Returns a list of duplicate movies from a src list"""
-    movies = read_movies(src)
-    duplicates = []
-    while movies:
-        movie = movies.pop()
-        if is_duplicate(movie, movies):
-            duplicates.append(movie)
+    movie_counter = Counter([x.lower() for x in read_movies(src)])
+    duplicates = [k for k, v in movie_counter.items() if v > 1]
     return duplicates
 
 
 def timeit_helper():
     """Part A:  Obtain some profiling measurements using timeit"""
-    # YOUR CODE GOES HERE
+    n = 8
+    r = 6
+    t = timeit.Timer(lambda: find_duplicate_movies('movies.txt'))
+    result = t.repeat(repeat=r, number=n)
+    result = [x / n for x in result]
+    print'Best time across {} repeats of {} runs per repeat: {} sec'.format(
+        r, n, min(result))
 
 
 def main():
     """Computes a list of duplicate movie entries"""
-    result = find_duplicate_movies('movies.txt')
-    print('Found {} duplicate movies:'.format(len(result)))
-    print('\n'.join(result))
+    find_duplicate_movies('movies.txt')
+    timeit_helper()
+#     result = find_duplicate_movies('movies.txt')
+#     print('Found {} duplicate movies:'.format(len(result)))
+#     print('\n'.join(result))
 
 
 if __name__ == '__main__':
