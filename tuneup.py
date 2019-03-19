e057674515c929cf5bcd4b485b572f782edf192f
Jennifer Browning <browningjen@yahoo.com>
2019-03-19 14:57:38 -0400

    added decorator function

--- tuneup.py
+++ tuneup.py
@@ -8,19 +8,18 @@ import cProfile
 import pstats
 import timeit
 
+#
+
 
 def profile(func):
     """A function that can be used as a decorator to measure performance"""
-    # cProfile.run(pr_func)
-    def profile_function(*args, **kwargs):
-        pr = cProfile.Profile()
-        pr.enable()
-        pr_func = func(*args, **kwargs)
-        pr.disable()
-        ps = pstats.Stats(pr).sort_stats('cumulative')
-        ps.print_stats()
-        return pr_func
-    return profile_function
+    def wrapper():
+        prof = cProfile.Profile()
+        prof.enable()
+        prof.runcall(func)
+        prof.disable()
+      return wrapper
+
 
 
 def read_movies(src):
@@ -38,26 +37,34 @@ def is_duplicate(title, movies):
     return False
 
 
-@profile
+
 def find_duplicate_movies(src):
     """Returns a list of duplicate movies from a src list"""
     movies = read_movies(src)
     duplicates = []
-    return set([movie for movie in movies if movies.count(movie) > 1])
+    while movies:
+        movie = movies.pop()
+        if is_duplicate(movie, movies):
+            duplicates.append(movie)
+    return duplicates
 
 
 def timeit_helper():
     """Part A:  Obtain some profiling measurements using timeit"""
-    t = min(timeit.Timer('find_duplicate_movies("movies.txt")', setup='from __main__ import find_duplicate_movies').repeat(
-        repeat=7, number=3))
-    print "Best time across 7 repeats of 3 runs per repeat: {} sec".format(t)
+    number = 10
+    repeat = 3
+    t = timeit.Timer(stmt=timeit_helper,
+                     setup='text = "sample string"; char = "g"')
+    result = t.repeat(repeat=7, number=3)
 
-
+@profile
 def main():
     """Computes a list of duplicate movie entries"""
     result = find_duplicate_movies('movies.txt')
     print('Found {} duplicate movies:'.format(len(result)))
     print('\n'.join(result))
+
+
 
 
 if __name__ == '__main__':
